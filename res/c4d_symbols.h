d66fe4a86ce9d000f2b4d4f9e458256901b3d31a
Frank Willeke <frank.willeke@laubwerk.com>
2017-02-28 19:55:47 +0100

    Added resource files

---
+++ res/c4d_symbols.h
@@ -0,0 +1,11 @@
+enum
+{
+	// string table definitions
+	IDS_PIPEOBJECT	= 10000,
+	IDS_PIPEOBJECT_PROFILE_OUTER,
+	IDS_PIPEOBJECT_PROFILE_INNER,
+	IDS_PIPEOBJECT_CONNECTOBJ,
+
+// End of symbol definition
+  _DUMMY_ELEMENT_
+};
